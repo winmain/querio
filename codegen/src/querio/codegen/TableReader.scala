69f32d09d2ea97bdd8eb7a3b1e565a6e19da0356
alexy <none@none>
2016-04-03 20:01:26 +0300

    Форматирование регулярок
    --HG--
    branch : alexy

--- codegen/src/querio/codegen/TableReader.scala
+++ codegen/src/querio/codegen/TableReader.scala
@@ -186,7 +186,17 @@ class TableReader(db: OrmDbTrait,lines: List[String]) {
 object TableReader {
   val extendsR = """(?s).*\) *(extends .*) *\{""".r
 
-  val tableR = """(?s)class +([^ \[]+Table)\(alias: *String\)\s+extends +Table\[[^\]]+\]\("[^"]+"\, *"[^"]+"\, *alias\, *(?:true|false)\, *(?:true|false) *\).*""".r
+  val tableR = """(?xs)
+    class\ +([^\ \[]+Table) # param: name
+    \(alias:\ *String\)     # (alias: String)
+    \s+extends\ +Table      # extends Table
+    \[[^\]]+\]              # [TR, MTR]
+    \("[^"]+"\,             # (_fullTableName,
+    \ *"[^"]+"\,            # _tableName,
+    \ *alias\,              # _alias
+    \ *(?:true|false)\,     # _needDbPrefix
+    \ *(?:true|false)\ *\)  # _escapeName
+    .*""".r
   def objectR(tableClassName: String) = ("""(?s)object +([^ \[]+)\s+extends +""" + Pattern.quote(tableClassName) + """\(null\).*""").r
   val tableFieldR = """(?x)
     val\ +([^\ ]+)                 # param: varName
@@ -214,7 +224,13 @@ object TableReader {
   val classR = """(?s)class +([^ \[\(]+).*(?:extends|with) +TableRecord.*""".r
   val classHeaderFieldR = """val +([^:]+): *(.+?)(?:,|\) +extends.*)""".r
   val classHeaderPrivateFieldR = """_([^:]+): *(.+?)(?:,|\) +extends.*)""".r
-  val classFieldR = """val +([^:]+): *(.+?) *= *[^ \.]+\.([^ \.]+)\.getValue\(rs\)""".r
+  val classFieldR = """(?x)
+    val\ +([^:]+)       # ignore val name
+    :\ *(.+?)           # param: scalaType
+    \ *=\ *[^\ \.]+\.
+    ([^\ \.]+)          # param: varName
+    \.getValue\(rs\)
+                    """.r
   val classTableR = "def +_table *=.*".r
   val classPrimaryKeyR = """def +_primaryKey *: *Int *=.*""".r
   val classToMutableString = "def toMutable:"
