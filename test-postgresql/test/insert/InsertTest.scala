cce340a1fcea352f42998d708308602b8268605f
izhleba <izhleba@gmail.com>
2016-05-15 17:27:16 +0300

    Insert test for multiple rows
    --HG--
    branch : test_kit

--- test-postgresql/test/insert/InsertTest.scala
+++ test-postgresql/test/insert/InsertTest.scala
@@ -1,17 +1,24 @@
 package insert
+import java.time.LocalDateTime
+
 import model.db.table.{MutableUser, User}
+import querio.ModifyData
 import test.{BaseScheme, DBUtil, DbTestBase}
 
-class InsertTest extends DbTestBase(BaseScheme.sql)  {
+class InsertTest extends DbTestBase(BaseScheme.sql) {
 
-  "User" should {
-//    "handle empty table" in {
-//      val result1 = db.query(_.select(User.email)
-//        from User
-//        limit 10
-//        fetch())
-//      result1 must beEmpty
-//    }
+  val mddt = new ModifyData {
+    override def dateTime: LocalDateTime = LocalDateTime.now()
+  }
+
+  "Table \"user\"" should {
+    "support access when  empty" in {
+      val result1 = db.query(_.select(User.email)
+        from User
+        limit 10
+        fetch())
+      result1 must beEmpty
+    }
 
     "support simple insert" in {
       val result1 = db.query(_.select(User.email)
@@ -34,5 +41,34 @@ class InsertTest extends DbTestBase(BaseScheme.sql)  {
         fetch())
       result2 must_== Vector(mail)
     }
+
+    "support insert of many rows" in {
+      val result1 = db.query(_.select(User.email)
+        from User
+        limit 10
+        fetch())
+      result1 must beEmpty
+      //
+      val mails = Range(1, 4).map {index =>
+        s"main$index@user.com"
+      }
+      val users = mails.map {mail =>
+        val user: MutableUser = DBUtil.dummyUser()
+        user.email = mail
+        user
+      }
+      db.dataTrReadCommitted(mddt) {implicit dt =>
+        users.foreach {user =>
+          db.insert(user)
+        }
+      }
+      //
+      val result2 = db.query(_.select(User.email)
+        from User
+        limit 10
+        fetch())
+      result2 must_== mails
+    }
+
   }
 }
