8845145cec9218772b591ed0c7d479e667e10bb5
winmain <d.winmain@gmail.com>
2016-06-27 09:31:33 +0700

    Version 0.4.7.1: Changed QuerioSQLException superclass to SQLException for better error handling.

--- src/querio/QuerioSQLException.scala
+++ src/querio/QuerioSQLException.scala
@@ -2,4 +2,4 @@ package querio
 import java.sql.SQLException
 
 class QuerioSQLException(message: String, cause: SQLException, val sql: String)
-  extends RuntimeException(message, cause)
+  extends SQLException(message, cause.getSQLState, cause.getErrorCode, cause)
