9ad72301f531ffa6197fd4846844ec70b3b6c16d
winmain <d.winmain@gmail.com>
2016-07-19 10:53:25 +0700

    Travis-ci deploy script

---
+++ travis-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function make-bintray-credentials {
+  if [ -z "$BINTRAY_USER" ] || [ -z "$BINTRAY_API_KEY" ]; then
+    echo "Env BINTRAY_USER and BINTRAY_API_KEY must be set"
+    exit -1
+  fi
+  mkdir $HOME/.bintray/
+  FILE=$HOME/.bintray/.credentials
+  cat <<EOF >$FILE
+realm = Bintray API Realm
+host = api.bintray.com
+user = $BINTRAY_USER
+password = $BINTRAY_API_KEY
+EOF
+  echo "Created ~/.bintray/.credentials file for $BINTRAY_USER"
+}
+
+
+if [ "$TRAVIS_BRANCH" == "production" ]; then
+  make-bintray-credentials
+  sbt publish
+fi
