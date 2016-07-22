ffecdb03584ed92914bd49fcb4d8c88257bb30b9
izhleba <izhleba@gmail.com>
2016-07-22 09:26:10 +0300

    Empty array test fot postgres bytea

--- test-postgresql/test/query/ByteaFieldUserTest.scala
+++ test-postgresql/test/query/ByteaFieldUserTest.scala
@@ -22,6 +22,17 @@ class ByteaFieldUserTest extends DbTestBase(
       rBytes must_== wBytes
     }
 
+    "support emty array in bytea field" in new FreshDB {
+      val user: MutableUser = DBUtil.dummyUser()
+      val wBytes: Array[Byte] = new Array[Byte](0)
+      user.bytearray = wBytes
+      db.dataTrReadCommittedNoLog {implicit dt =>
+        db.insert(user)
+      }
+      val rBytes = db.query(_.select(User.bytearray) from User fetch()).head
+      rBytes must_== wBytes
+    }
+
     "support None in  nullable bytea field" in new FreshDB {
       val user: MutableUser = DBUtil.dummyUser()
       user.bytearraynullable = None
